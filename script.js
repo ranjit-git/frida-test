7591792da621a53f8927a6e53f159d733bdd91bc
ranjit-git <pahan.ranjit0201@gmail.com>
2019-10-01 15:45:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Script loaded successfully ");
+Java.perform(function(){ 
+    console.log("Inside java perform function2");
+   
+   var my_class = Java.use("com.test.app.p025h4.p026g.C0811a"); 
+    
+    my_class.mo3581a.implementation = function(context){ 
+	   console.log("inside implimentation");
+    return true;
+    }
+
+
+});
